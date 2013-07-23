5b62eb8e5ae38c50e350ccea196b07a43d1f41a4
Simon Schubert <2@0x2c.org>
2013-07-23 16:43:33 +0200

    add hyde config

---
+++ .hyde.el
@@ -0,0 +1,6 @@
+(setq
+ hyde-post-extension "org"
+ hyde-source-dir "source"
+ hyde-posts-dir "source/_posts"
+ hyde-drafts-dir "source/_drafts"
+ hyde/jekyll-command "OCTOPRESS_ENV=preview rake generate")
