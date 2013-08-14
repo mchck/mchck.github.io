b74ef7b7982da027f08ab8f28cd306c4c20566d5
Simon Schubert <2@0x2c.org>
2013-08-14 11:22:13 +0200

    configure hyde for octopress draft mode

--- .hyde.el
+++ .hyde.el
@@ -2,5 +2,5 @@
  hyde-post-extension "org"
  hyde-source-dir "source"
  hyde-posts-dir "source/_posts"
- hyde-drafts-dir "source/_drafts"
+ hyde-drafts-dir "source/_posts"
  hyde/jekyll-command "OCTOPRESS_ENV=preview rake generate")
